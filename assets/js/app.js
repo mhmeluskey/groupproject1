44acb6c89f7e350652dcb021bbe9c1e973f48abf
Iankimble <38480036+Iankimble@users.noreply.github.com>
2018-09-20 20:30:37 -0400

    2nd ajax call (#7)

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,6 +37,7 @@ $("#get-recipes").on("click", function (event){
 
 
 // var search = foodInput;
+//var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/informationBulk?ids=933310%2C936707%2C721001%2C65597%2C163949/information?includeNutrition=false"
 var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=" + foodInput + "&limitLicense=false&number=5&ranking=1";
 
 
@@ -48,10 +49,51 @@ $.ajax({
     console.log("hello");
     console.log(response);
     displayRecipes(response);
+
+    var bulkRecipes = ""
+    for(var i= 0; i<response.length; i++){
+        if (i === response.length-1) {
+            bulkRecipes += response[i].id
+        } else {
+            bulkRecipes += response[i].id + "%2C";
+        }
+        
+    //     var recipe ="https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/food/products/"+response[0].id+"/information?includeNutrition=false";
+    //     //recipe
+        
+    //     $.ajax({
+    //         url: recipe,
+    //         method: "GET",
+    //         header: {"Accept":'application/json',"X-Mashape-Key": "ZFmvhX3Np4mshkom23Cm7BKRqwYFp1LChSXjsnKEoMpsW5hD8n"}
+    //     }).then(function(recipe){
+    //         console.log(recipe);
+    //     });
+        
+    //    console.log(recipe);
+    }
+
+    recipe ="https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/informationBulk?ids="+bulkRecipes+"&includeNutrition=false";
+    console.log(recipe);
+
+
+    //recipe
+//var recipe ='https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/informationBulk?ids='+response[i].id +'&includeNutrition=false' 
+
+$.ajax({
+    url: recipe,
+    method: "GET",
+    headers: {"Accept": 'application/json',"X-Mashape-Key": "ZFmvhX3Np4mshkom23Cm7BKRqwYFp1LChSXjsnKEoMpsW5hD8n"}
+}).then(function(recipe){
+    console.log(recipe);
+});
+
+
 }).catch(function(error){
     console.log(error);
 });
 
-
 });
+
+
+
 
