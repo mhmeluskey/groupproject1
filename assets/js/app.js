f5110dc188608da0f1a30e23e42cf4610e76e8f5
gkpeel <gkpeel@gmail.com>
2018-09-22 09:33:32 -0400

    consolidated files/style and improved the modal display

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,69 @@
 function buildRecipeCard(responseObj) {
 
-    var recipeContainer = $('<div class="col-sm-6 col-md-4 col-lg-3"><div class="ingredient-card">');
-    var recipeCard = $('<div class="ingredient-card">');
-    recipeContainer.append(recipeCard);
-    var recipeContent = '<img class="mx-auto img-fluid d-block" src="'+responseObj.image+'"><h3>'+responseObj.title+'</h3><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#recipe-'+Number(responseObj.id)+'">See Recipe</button>';  
-    recipeCard.append(recipeContent);
+    var recipeContainer = $('<div class="col-sm-6 col-md-4 col-lg-3 mb-lg-5 mb-3">');
+
+    var recipeContent = '<div class="recipe-card">';
+        recipeContent += '<img class="mx-auto img-fluid d-block" src="'+responseObj.image+'">';
+        recipeContent += '<div class="recipe-details-container p-lg-3 p-1">';
+        recipeContent += '<h4 class="mb-4">'+responseObj.title+'</h4>';
+            recipeContent += '<div class="text-center">'
+                recipeContent += '<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#recipe-'+Number(responseObj.id)+'">See Recipe</button>';  
+            recipeContent += '</div>';
+        recipeContent += '</div>';
+    recipeContent += '</div>';
+    
+    recipeContainer.append(recipeContent);
     return recipeContainer;
 
 }
 
+function createRecipeQuery(responseObj) {
+    var bulkRecipes = "";
+    for(var i= 0; i<responseObj.length; i++){
+        if (i === responseObj.length-1) {
+            bulkRecipes += responseObj[i].id
+        } else {
+            bulkRecipes += responseObj[i].id + "%2C";
+        }
+    }
+    return bulkRecipes;
+}
+
 function buildRecipeModal(responseObj) {
+
     for (var i=0; i<responseObj.length; i++){
+
+        console.log(responseObj[i]);
 
         var modalId = "recipe-" + responseObj[i].id;
 
-        var recipeModal = '<div class="modal fade bd-example-modal-lg" id="'+modalId+'" tabindex="-1" role="dialog"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">'+responseObj[i].title+'</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><p>'+responseObj[i].instructions+'</p></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button></div></div></div></div>';
+        var recipeModal = '<div class="modal fade bd-example-modal-lg" id="'+modalId+'" tabindex="-1" role="dialog">';
+            recipeModal += '<div class="modal-dialog" role="document">';
+                recipeModal += '<div class="modal-content">'
+                    recipeModal += '<div class="modal-header">';
+                        recipeModal += '<h5 class="modal-title">'+responseObj[i].title+'</h5>';
+                        recipeModal += '<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>'
+                    recipeModal += '</div>';
+                    recipeModal += '<div class="modal-body">';
+                        recipeModal += '<div class="container">';
+                            recipeModal += '<div class="row">';
+                                recipeModal += '<div class="col-sm-4">';
+                                    recipeModal += '<img class="img-fluid d-block" src="'+responseObj[i].image+'">';
+                                    recipeModal += '<div class="ingredients-list">'+ ingredientsBuild(responseObj[i].extendedIngredients) +'</div>';
+                                recipeModal += '</div>';
+                                recipeModal += '<div class="col-sm-8">';
+                                    recipeModal += '<p>'+ instructionsBuild(responseObj[i].analyzedInstructions[0].steps) +'</p>';
+                                recipeModal += '</div>';
+                            recipeModal += '</div>';
+                            recipeModal += '<div class="row my-4">';
+                                recipeModal += '<div class="col-sm-12 text-center">';
+                                    recipeModal += '<button class="btn btn-primary btn-lg" data-nutrition-val="#">Eat it On</button><button class="btn btn-warning btn-lg ml-4">Work It Off</button>'
+                        recipeModal += '</div>';
+                    recipeModal += '</div>';
+                    recipeModal += '<div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button></div>';
+                recipeModal += '</div>';
+            recipeModal += '</div>';
+        recipeModal += '</div>';
 
         $("#recipe-modal").append(recipeModal);
 
@@ -36,19 +85,43 @@ function displayRecipes(response) {
 
 }
 
+function ingredientsBuild(responseArr) {
+    retval = "";
+    for (var i=0; i<responseArr.length; i++) {
+        var ingredient = '<p>';
+        ingredient += responseArr[i].measures.us.amount;
+        if (responseArr[i].measures.us.unitLong !== "") {
+            ingredient += ' ' + responseArr[i].measures.us.unitLong;
+        }
+        ingredient += ' ' + responseArr[i].name;
+        ingredient += '</p>';
+        retval += ingredient;
+    }
+    return retval;
+}
+
+function instructionsBuild(responseArr) {
+    retval = "";
+    for (var i=0; i<responseArr.length; i++) {
+        var instruction = '<p>';
+        instruction += responseArr[i].number + '.) ';
+        instruction += '<span class="recipe-instruction">' + responseArr[i].step + '</span>';
+        instruction += '</p>';
+        retval += instruction;
+    }
+    return retval;
+}
+
 
 $("#get-recipes").on("click", function (event){
-    console.log("ive been clicked");
     $("#ingredient-search-results").empty();
     var foodInput = $("#ingredient-search-input").val().trim();
-    console.log(foodInput);
     //takes away button default behavior 
     event.preventDefault();
     //empty out the value after clicking
-    $("#food").on("#get-recipes").val("");
+    $("#ingredient-search-input").on("#get-recipes").val("");
 
     var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?fillIngredients=false&ingredients=" + foodInput + "&limitLicense=false&number=12&ranking=1";
-
 
     $.ajax({
 
@@ -57,19 +130,10 @@ $("#get-recipes").on("click", function (event){
         headers: {"Accept": "application/json", "X-Mashape-Key": "ZFmvhX3Np4mshkom23Cm7BKRqwYFp1LChSXjsnKEoMpsW5hD8n"}
 
     }).then(function(response){
-        console.log(response);
+
         displayRecipes(response);
 
-        // create bulk recipe query based on response recipe ID
-        var bulkRecipes = ""
-        for(var i= 0; i<response.length; i++){
-            if (i === response.length-1) {
-                bulkRecipes += response[i].id
-            } else {
-                bulkRecipes += response[i].id + "%2C";
-            }
-        }
-
+        var bulkRecipes = createRecipeQuery(response);
         var recipeQuery = "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/informationBulk?ids="+bulkRecipes+"&includeNutrition=false";
 
         $.ajax({
@@ -79,9 +143,7 @@ $("#get-recipes").on("click", function (event){
             headers: {"Accept": 'application/json',"X-Mashape-Key": "ZFmvhX3Np4mshkom23Cm7BKRqwYFp1LChSXjsnKEoMpsW5hD8n"}
 
         }).then(function(response){
-
             buildRecipeModal(response);
-
         });
 
     }).catch(function(error){
